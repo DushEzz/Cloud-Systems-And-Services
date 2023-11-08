9f960c32f52c4c520f9bc51062494edac781b1e4
Andrei <dushez.baranov@gmail.com>
2023-11-08 15:43:38 +0300

    add lab-3 project files

---
+++ lab-3/app.py
@@ -0,0 +1,37 @@
+import argparse
+
+from flask import (
+    Flask,
+    Response,
+)
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index() -> Response:
+    return Response(f"Hello, {app.config['say_hello_to']}!")
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--say-hello-to", default="Pelmeshki")
+
+    return parser.parse_args()
+
+
+def create_app() -> Flask:
+    args = parse_args()
+    app.config["say_hello_to"] = args.say_hello_to
+
+    return app
+
+
+def run_app() -> None:
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000)
+
+
+if __name__ == '__main__':
+    run_app()
